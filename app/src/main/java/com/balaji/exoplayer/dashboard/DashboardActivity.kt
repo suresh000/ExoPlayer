23a3253d6449eff4af7b76b6589ae222d290af9d
SSureshC <sureshchandak1@gmail.com>
2022-11-05 20:03:09 +0530

    Request runtime permission

--- app/src/main/java/com/balaji/exoplayer/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/balaji/exoplayer/dashboard/DashboardActivity.kt
@@ -1,6 +1,10 @@
 package com.balaji.exoplayer.dashboard
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.balaji.exoplayer.R
@@ -11,12 +15,16 @@ import com.balaji.exoplayer.videos.VideosFragment
 
 class DashboardActivity : BaseActivity() {
 
+    private val PERMISSION_REQUEST_CODE by lazy { 11 }
+
     private lateinit var binding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        requestRuntimePermission()
 
         binding.bottomNavigation.setOnItemSelectedListener {
             return@setOnItemSelectedListener when (it.itemId) {
@@ -32,6 +40,26 @@ class DashboardActivity : BaseActivity() {
             }
         }
         binding.bottomNavigation.selectedItemId = R.id.videos
+    }
+
+    private fun requestRuntimePermission() : Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "Storage permission not granted", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+            }
+        }
     }
 
     override fun getCurrentFragment(): Fragment? {
